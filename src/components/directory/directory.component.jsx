fa6908c68277dba43beb54cf020c100e4b56f8d6
Karthik Krishnan <karthikkrishnan@Karthiks-MacBook-Pro.local>
2020-07-14 23:25:58 +0530

    routing implemented with urls

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -8,17 +8,15 @@ class Directory extends React.Component {
 
     constructor() {
         super();
-        this.state = {
-            sections: sections
-        }
+        this.state = { sections }
     }
 
     render() {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
