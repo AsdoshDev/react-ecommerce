50a1525eeb517f83390eb126c6c042f84ee0c701
Karthik Krishnan <karthikkrishnan@Karthiks-MacBook-Pro.local>
2020-08-22 20:04:44 +0530

    use promise instead of rxjs

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -19,15 +19,29 @@ class ShopPage extends React.Component {
     componentDidMount() {
         const collectionRef = firestore.collection('collections');
         const { updateCollection } = this.props;
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
-            const collectionsMap = await convertCollectionsSnapshotToMap(snapshot);
+        //using rx js of firestore
+        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async (snapshot) => {
+        //     const collectionsMap = await convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollection(collectionsMap);
+        //     this.setState({ loading: false });
+        // })
+
+        //using promise
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollection(collectionsMap);
             this.setState({ loading: false });
-        })
+        });
+
+        //using fetch
+        console.log("using fetch")
+        // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-209d5/databases/(default)/documents/collections')
+        //     .then(res => res.json())
+        //     .then(collections => console.log(collections));
     }
 
     componentWillUnmount() {
-        this.unsubscribeFromSnapshot();
+        // this.unsubscribeFromSnapshot();
     }
 
     render() {
